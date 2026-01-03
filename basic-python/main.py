0a2e6b54d6efed96a5efea093214456859233e13
chukyobi <josephclinton.obi@gmail.com>
2026-01-03 03:22:20 -0800

    basic - 03/01/2026:12-22pm

--- basic-python/main.py
+++ basic-python/main.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+import math
+
 x = 10
 y = 11
 z = x + y
@@ -9,4 +12,34 @@ totalDistance = 435
 
 time = totalDistance/mph
 
-print(time, f"and it can be rounded up to {round(time, 3)}: in 3 decimal places")
+j = Decimal('0.1')+ Decimal('0.2')
+
+print(time, f"and it can be rounded up to {round(time, 3)}: in 3 decimal places", j)
+print(f"{time: .1f}")
+
+if(math.isclose((6-5.7), 0.3)):
+    print("true")
+
+text = "chukwudi"
+
+new_text = text[0] + text[3] + text[5]
+
+print(f"the new text is {new_text}")
+
+sentence = "python is a language"
+
+print(sentence[0:8])
+
+address = ''' 43 omoba murphy adetoro street, lekki "county home"
+lekki,
+lagos,
+NG'''
+print(address)
+
+items =["bread", "orange", "mango", "salt"]
+items.append("tomatoes")
+
+items.insert(2, "avocado")
+
+print(items)
+
