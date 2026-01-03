ddc0779457fc2b4b208a42b1f2f47d4f981d8f0e
chukyobi <josephclinton.obi@gmail.com>
2026-01-03 05:01:05 -0800

    basic-python: 03/01/2026, 2pm

---
+++ basic-python/02_Build-Calculator.py
@@ -0,0 +1,10 @@
+#Learning about Input abd Data Conversion
+
+str_num_a = input('Enter a First Number: ')
+str_num_b = input('Enter a Second Number: ')
+
+num_a = int(str_num_a)
+num_b = int(str_num_b)
+
+total = num_a + num_b
+print(f" the sum of num_a and num_b is: {total}")
