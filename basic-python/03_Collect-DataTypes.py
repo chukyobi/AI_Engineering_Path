75c5f4265b8f2a723930a048d339ba3c91bd8ac8
chukyobi <josephclinton.obi@gmail.com>
2026-01-03 14:34:03 -0800

    basic-python: 03/01/2026, 11:33pm

--- basic-python/03_Collect-DataTypes.py
+++ basic-python/03_Collect-DataTypes.py
@@ -122,3 +122,124 @@ my_list = ['wall', 'floor', 'roof', 'ceiling']
 for i in my_list:
     print(i)
     print(i)
+
+
+
+
+
+
+
+# Tuples (Immutable Ordered Collection) - Not popularly used
+# ----------------------------------------------------------
+
+empty_tuple = () # or tuple()
+list_data = [0,1,2,3,4,5,6,7,8,9]  # List can be modified  (Mutable)
+data = (0, 1, 2, 3, 4, 5, 7, 0, 3, 4, 8)  #Tuples can't be modified (Immutable)
+data_pts = ((0,0,0), (1,2,3), (2,4,6))   # Nested Tuples
+
+# Get single List Item
+print(data[0])
+print(data[2])
+print(data[5])
+print(data_pts[1])
+
+print(sorted(data)) # Sorted on a tuple sorts the tuple and turns it into a list
+#if you want to convert the list back to a tuple
+print(tuple(sorted(data)))  #converts list into tuple
+
+# Tuple methods (Built-In Functionality)
+data = (0, 1, 2, 3, 4, 5, 7, 0, 3, 4, 8)
+print(data.count(3))
+print(data.index(3))
+
+
+
+
+
+
+# SETS (Unique, Mutable Unordered Collection)
+# ----------------------------------------------------------
+
+new_set = set()
+set_items = { 10, 20, 30, 10, 20, 'AB', 'BA', 'AB', True, True, False}
+
+print(set_items) # P.S: don't always rely on the order of your sets, it will always print at random no mater how many times it ran
+
+# Convert List/Tuples to SETS
+
+data_list = ['amaka', 'joy', 'emeka', 'chukwudi', 'joy', 'oge', 'amaka', 'chioma']
+data_set = set(data_list)
+data_set_list = list(data_set)
+
+print(data_list)
+print(data_set)
+print(data_set_list)
+
+# SET Methods (Built-In functionality)
+
+set_items = { 10, 20, 30, 10, 20, 'AB', 'BA', 'AB', True, True, False}
+
+# copy_set = set_items.copy()  #copies the set to have an independent copy
+# removed = set_items.pop()  #removes and returns a random element in the set
+# set_items.clear() #removes all elements
+set_items.remove(10) #removes element but throws an error if it does not exist
+set_items.discard(3) #removes element, no error if it does not exist
+set_items.add(15)   #add a single element
+
+print(set_items)
+
+
+# SET Methods: Compare methods
+a = {1,2,3,4}
+b= {3,4,5,6}
+
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+print(a.symmetric_difference(b))
+print(a.issubset(b))
+print(a.issuperset(b))
+
+
+# Popular Functions with Sets
+a = {1,2,3,4,5}
+
+print(len(a)) # Count elements
+print(min(a)) #Minimum
+print(max(a)) #Maximum
+print(sum(a)) #Sum
+print(sorted(a)) # sorted list
+
+
+
+
+# DICTIONARIES ---- (Mapped Ordered* Mutable Collection)
+# --------------------------------------------------------
+
+empty_dict = {}
+dict_example = {
+    'key1': 'value1',
+    'key2': 'value2'
+}
+
+#Get Items
+print(dict_example['key1'])
+print(dict_example.get('key2'))
+print(dict_example.setdefault('key1', 'non-value'))
+
+
+# Adjust and modify data
+#-----------------------
+dict_example['key1'] = "my name is chukwudi"
+dict_example['key3'] = "value3"
+print(dict_example)
+
+print('key1' in dict_example)  #check for keys if they exist in dictionary
+print('key4' in dict_example)
+
+
+#Dict Methods ( Built In Functionality)
+
+print(list(dict_example.keys()))
+print(list(dict_example.values()))
+print(list(dict_example.items()))
