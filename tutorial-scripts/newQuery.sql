d9c6dfa85ba767da2952fb444f5cf73142a9d90c
chukyobi <josephclinton.obi@gmail.com>
2026-03-05 01:27:31 -0800

    Add sql-ultimate-course folder

---
+++ tutorial-scripts/newQuery.sql
@@ -0,0 +1,24 @@
+USE MyDatabase;
+
+SELECT * FROM [dbo].[customers];
+
+SELECT * FROM 
+[dbo].[orders];
+
+SELECT 
+       [country],
+       [score]
+FROM   [dbo].[customers];
+
+SELECT 
+        [first_name],
+        [score],
+        [country]
+FROM [dbo].[customers]
+WHERE [country] = 'Germany' AND [score] > 400;
+
+
+SELECT * 
+    FROM [dbo].[customers]
+    WHERE [country] = 'Germany'
+    ORDER BY [country], [score] DESC;
